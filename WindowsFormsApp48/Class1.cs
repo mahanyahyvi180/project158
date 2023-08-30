e9d7dbaafc0758f25f0e94e3b3a704dfa79dd9e1
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-30 19:49:03 +0330

    Add files via upload

---
+++ WindowsFormsApp48/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp48
+{
+    namespace teacher
+    {
+        public class cEmployee
+        {
+            public string identity;
+            public string firstname;
+            public string lastname;
+
+            public cEmployee()
+            {
+                firstname = "ali";
+                lastname = "razavi";
+            }
+
+            public string Concatinate()
+            {
+                return firstname + ' ' + lastname;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp48/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp48.teacher;
+
+namespace WindowsFormsApp48
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cEmployee y = new cEmployee();
+        }
+    }
+}
